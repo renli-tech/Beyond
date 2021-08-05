d89ef77a83a25e845c719c2186455b8fa9b0cf9f
Ope Abidemi Harry <58413594+OpeAbidemi@users.noreply.github.com>
2021-08-05 07:30:25 +0100

    Update createComponent.ts

--- packages/system/src/createComponent.ts
+++ packages/system/src/createComponent.ts
@@ -10,8 +10,8 @@ export function createComponent<TTag, OurProps>(
   view: React.FC<{}>,
   { props, displayName }: Options<TTag, OurProps>
 ): React.FunctionComponent<Props<TTag, OurProps>> {
-  const ResultComponent: React.FC<Props<TTag, OurProps>> = () => {
-    return React.createElement(view)
+  const ResultComponent: React.FC<Props<TTag, OurProps>> = (props) => {
+    return React.createElement(view,props,props.children)
   };
 
   ResultComponent.displayName = displayName || "BeyondComponent";
