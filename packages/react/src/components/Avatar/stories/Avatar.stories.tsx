79301956489e86c0e1a517fba121f91571a111e5
Muiz Ogundiran <muizakanji@gmail.com>
2021-08-16 12:46:29 +0000

    chore: updated avatar components

--- packages/react/src/components/Avatar/stories/Avatar.stories.tsx
+++ packages/react/src/components/Avatar/stories/Avatar.stories.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Meta } from "@storybook/react";
 import { Avatar } from "../Avatar";
+import { Container } from "../../Container";
 export default {
   title: "Avatar",
   component: Avatar
@@ -9,9 +10,22 @@ export default {
 
 export const Basic = () => {
   return (
-    <Avatar
-      src='https://avatars.githubusercontent.com/u/80798883?s=200&v=4'
-      size='4xl'
-    />
+    <Container bgcolor='red'>
+      <Avatar
+        src='https://avatars.githubusercontent.com/u/80798883?s=200&v=4'
+        size='lg'
+        title='Renli Logo'
+      />
+      <Avatar
+        src='https://avatars.githubusercontent.com/u/80798883?s=200&v=4'
+        size='4xl'
+        title='Renli Logo'
+      />
+      <Avatar
+        src='https://avatars.githubusercontent.com/u/80798883?s=200&v=4'
+        size='8xl'
+        title='Renli Logo'
+      />
+    </Container>
   );
 };
