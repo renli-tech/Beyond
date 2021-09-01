cb43adabf5f41a9120d79a66423b2c156d3938a1
Ope Abidemi Harry <opeabidemi@gmail.com>
2021-09-01 19:41:32 +0000

    fix(shared): updated useColor hook

--- packages/shared/src/hooks/useColor.ts
+++ packages/shared/src/hooks/useColor.ts
@@ -1,48 +1,25 @@
 // import * as React from "react";
-import { ColorName, colors, getColor, Theme } from "@beyond-ui/theme";
+import { ColorName, colors, extractColor } from "@beyond-ui/theme";
 import React from "react";
+import { merge } from "@beyond-ui/utils";
 import { useTheme } from "./useTheme";
+import { Property } from "csstype";
 
-// TODO: Complete this fuction
-export const extractColor = (name: string): string => {
-  return name;
-};
-
-export const useColor = (name: ColorName | string): string | undefined => {
+export const useColor = (
+  name: ColorName | Property.Color
+): string | undefined => {
   const theme = useTheme();
-  const colorExistInTheme = React.useCallback(
-    (colorName, theme: Theme): boolean => {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      if (colorName in theme.colors!) {
-        return true;
-      }
 
-      return false;
-    },
-    [theme]
-  );
-
-  const [color, setColor] = React.useState<string | undefined>(name);
+  const [color, setColor] = React.useState<string | undefined>("");
 
   React.useEffect(() => {
     // checking if color already exist theme
     // user-defined theme colors are used instead of the base theme
-    if (theme?.colors && colorExistInTheme(name, theme)) {
-      setColor(theme.colors[name] as string);
-      return;
-    }
+    const val = extractColor(name, merge(colors, theme?.colors || {}));
 
-    if (name in colors) {
-      // console.log("found");
-      const n = getColor(name as ColorName);
-      setColor(n);
-      return;
-    }
-
-    return setColor(name);
+    return setColor(val);
   }, [theme]);
 
   // console.log(theme);
-
   return color;
 };
